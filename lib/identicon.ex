2cd3ffe747e89393feeaf4322efe3cc3221a4112
chamatht <cham@bits64.com>
2019-12-16 14:07:05 +0100

    add save function

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -1,11 +1,13 @@
 defmodule Identicon do
-  def main(input) do
-    input
+  def main(args) do
+    args
     |> hash_input
     |> pick_color
     |> build_grid
     |> filter_odd_squares
     |> build_pixel_map
+    |> draw_image
+    |> save_image(args)
   end
 
   def hash_input(input) do
@@ -49,6 +51,21 @@ defmodule Identicon do
     end
 
     %Identicon.Image{image | pixel_map: pixel_map}
+  end
+
+  def draw_image(%Identicon.Image{color: color, pixel_map: pixel_map}) do
+    image = :egd.create(250, 250)
+    fill = :egd.color(color)
+
+    Enum.each pixel_map, fn {start, stop} ->
+      :egd.filledRectangle(image, start, stop, fill)
+    end
+
+    :egd.render(image)
+  end
+
+  def save_image(egd_image, filename) do
+    File.write("#{filename}.png", egd_image)
   end
 
 end
--- mix.exs
+++ mix.exs
@@ -7,6 +7,7 @@ defmodule Identicon.MixProject do
       version: "0.1.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
+      escript: [main_module: Identicon],
       deps: deps()
     ]
   end
